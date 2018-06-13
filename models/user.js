8389fd530a4a39ec3829e64909c199098f0bf648
gabrielchase <gchase.patron@gmail.com>
2018-06-13 21:27:11 +0800

    User model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    created_on: { type: String },
+    modified_on: { type: String },
+    first_name: { type: String },
+    last_name: { type: String },
+    birthday: { type: String },
+    occupation: { type: String }
+})
+
+module.exports = mongoose.model('User', User)
