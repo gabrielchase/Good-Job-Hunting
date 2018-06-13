603b19bbbf131ef35deada717e67fcb55735ae80
gabrielchase <gchase.patron@gmail.com>
2018-06-13 21:26:56 +0800

    Success and fail wrappers for API responses

---
+++ utils.js
@@ -0,0 +1,19 @@
+const logger = require('winston')
+
+module.exports = {
+    success: (res, data) => {
+        const obj = {
+            success: true,
+            data: data
+        }
+        res.json(obj)
+    },
+    fail: (res, err) => {
+        logger.error(err)
+        const obj = {
+            success: false,
+            message: err.message
+        }
+        res.json(obj)
+    }
+}
