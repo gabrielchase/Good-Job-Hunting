99997ad3301b5d030b923f5416111291003d4e89
gabrielchase <gchase.patron@gmail.com>
2018-06-13 22:46:49 +0800

    Make user email unique

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const User = mongoose.Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     created_on: { type: String },
     modified_on: { type: String },
