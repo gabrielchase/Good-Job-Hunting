aea660166f27577473f517f84f4469701daa4f97
gabrielchase <gchase.patron@gmail.com>
2018-06-14 23:10:42 +0800

    POST job endpoint

--- routes/job_routes.js
+++ routes/job_routes.js
@@ -1,5 +1,7 @@
 const { success, fail } = require('../utils')
 
+const Job = require('../models/job')
+
 module.exports = (app, logger, config) => {    
     app.get('/api/user/:user_id/jobs', checkJWT, async (req, res) => {
         try {
@@ -7,6 +9,19 @@ module.exports = (app, logger, config) => {
             console.log(req.user)
             success(res)
         } catch (err) {
+            fail(res, err)
+        }
+    })
+
+    app.post('/api/job', checkJWT, async (req, res) => {
+        try {
+            req.body.created_on = new Date()
+            req.body.due_date = new Date(req.body.due_date)
+            req.body.user_id = req.user._id 
+            const job = new Job(req.body)
+            job.save()
+            success(res, job)
+        } catch (error) {
             fail(res, err)
         }
     })
