663a3772fbfb90ffc29f1554b5a52216554f5d0f
gabrielchase <gchase.patron@gmail.com>
2018-07-02 23:15:39 +0800

    Fix user update

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -17,14 +17,16 @@ module.exports = (app, logger) => {
 
     app.put('/api/user/:user_id', checkJWT, checkUser, async (req, res) => {
         const { user_id } = req.params
-        const { first_name, last_name, birthday, occupation } = req.body
+        const { first_name, last_name, birthday, city, country, occupation } = req.body
         try {
             const newUserData = {
-                first_name: first_name,
-                last_name: last_name, 
+                first_name,
+                last_name, 
+                city,
+                country,
                 birthday: new Date(birthday),
                 modified_on: new Date(),
-                occupation: occupation
+                occupation
             }
             const updatedUser = await User.findByIdAndUpdate(user_id, { $set: newUserData }, { new: true, multi: true})
             success(res, updatedUser)
