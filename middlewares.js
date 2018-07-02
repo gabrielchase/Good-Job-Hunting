e683aa63f2aaa100e0d40a05f2cc254419a8457b
gabrielchase <gchase.patron@gmail.com>
2018-07-02 23:52:40 +0800

    Middleware for deleted user and job user

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken')
 
+const Job = require('./models/job')
+const User = require('./models/user')
+
 const { fail } = require('./utils')
 const { JWT_SECRET } = require('./config/config')(require('winston'))
 
@@ -15,13 +18,24 @@ module.exports = {
             fail(res, err)
         }
     },
-    checkUser: async(req, res, next) => {
+    checkUser: async (req, res, next) => {
         const { _id } = req.user
         const { user_id } = req.params
+        
+        const user = await User.findById(user_id) 
+        if (user.deleted_on) fail(res, new Error('User deleted'))
 
         if (_id !== user_id) 
             fail(res, new Error('Unauthorized'))
         else    
             next()
+    },
+    checkJobUser: async (req, res, next) => {
+        const { _id } = req.user
+        const { job_id } = req.params
+
+        const job = await Job.findById(job_id)
+        if (job.user_id === _id) next()
+        else fail(res, new Error('Unauthorized'))
     }
 }
