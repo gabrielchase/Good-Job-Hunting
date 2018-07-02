01ac0f7b57087631d1b40455630b48e2d1bff583
gabrielchase <gchase.patron@gmail.com>
2018-07-02 22:02:57 +0800

    Fix auth_routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -4,21 +4,22 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 const { success, fail } = require('../utils')
+const { JWT_SECRET, SALT_ROUNDS } = require('../config/config')(require('winston'))
 
 
 module.exports = (app, logger) => {
     app.post('/api/register', async (req, res) => {
         const { email, password } = req.body 
         try {
-            const hashedPassword = await bcrypt.hash(password, config.SALT_ROUNDS)
-            const user = new User({ email, password: hashedPassword })
+            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+            const user = await new User({ email, password: hashedPassword })
             await user.save()
-            const userJson = {
+            const userJSON = {
                 _id: user._id,
                 email: user.email,
                 created_on: user.created_on
             }
-            success(res, userJson)
+            success(res, userJSON)
         } catch (err) {
             fail(res, err)
         }
@@ -30,13 +31,13 @@ module.exports = (app, logger) => {
             const user = await User.findOne({ email })
             const match = await bcrypt.compare(password, user.password)
             if (match) {
-                const signedJwt = jwt.sign({ _id: user._id, email: email }, config.JWT_SECRET, { expiresIn: '1d' })
-                const authJson = {
+                const signedJwt = jwt.sign({ _id: user._id, email: email }, JWT_SECRET, { expiresIn: '1d' })
+                const authJSON = {
                     _id: user._id,
                     email: email, 
                     token: signedJwt
                 }
-                success(res, authJson)
+                success(res, authJSON)
             } else {
                 throw new Error(`Error authenticating ${email}`)
             }
