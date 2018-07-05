72beb5807af29c9e42c9ca6ebb4ec63ec92dcac8
gabrielchase <gchase.patron@gmail.com>
2018-07-06 00:37:51 +0800

    GET, PUT, and DELETE job endpoint tests

--- tests/job_test.js
+++ tests/job_test.js
@@ -6,7 +6,7 @@ const Job = require('../models/job')
 
 const app = require('../app')
 
-const { user_email_password_data, jobs_fixture } = require('./fixtures.json')
+const { user_email_password_data, jobs_fixture, job_update_fixture } = require('./fixtures.json')
 const { handleSalary } = require('../utils')
 
 describe('Job tests', () => {
@@ -76,6 +76,7 @@ describe('Job tests', () => {
             assert.equal(userJobs[i].country, jobInfo.country)
             assert.equal(userJobs[i].city, jobInfo.city)
             assert.equal(userJobs[i].notes, jobInfo.notes)
+            assert.equal(userJobs[i].link, jobInfo.link)
             assert.equal(userJobs[i].position, jobInfo.position)
             assert.equal(userJobs[i].priority, jobInfo.priority)
             let { currency, value } = handleSalary(jobInfo.salary)
@@ -84,6 +85,66 @@ describe('Job tests', () => {
             assert.equal(userJobs[i].due_date.toISOString(), new Date(jobInfo.due_date).toISOString())
             assert.equal(userJobs[i].user_id, fixtureUserId)
         }
+    })
+
+    it('Should get a job', async () => {
+        const userJobs = await Job.find({})
+        const job = userJobs[0]
+        const { statusCode, body } = await request(app)
+                                        .get(`/api/job/${job._id}`)
+                                        .set('Authorization', `Bearer ${token}`)
+
+        assert.equal(statusCode, 200)
+        assert.equal(body.data._id, job._id)
+        assert.equal(body.data.company, job.company)
+        assert.equal(body.data.country, job.country)
+        assert.equal(body.data.city, job.city)
+        assert.equal(body.data.notes, job.notes)
+        assert.equal(body.data.position, job.position)
+        assert.equal(body.data.priority, job.priority)
+        assert.equal(body.data.salary_currency, job.salary_currency)
+        assert.equal(body.data.salary, job.salary)
+        assert.equal(new Date(body.data.due_date).toISOString(), job.due_date.toISOString())
+        assert.equal(body.data.user_id, fixtureUserId)
+    })
+
+    it('Should update a job', async () => {
+        let userJobs = await Job.find({})
+        let job = userJobs[0]
+        const { statusCode, body } = await request(app)
+                                            .put(`/api/job/${job._id}`)
+                                            .set('Authorization', `Bearer ${token}`)
+                                            .send(job_update_fixture)
+
+        assert.equal(statusCode, 200)
+        assert.equal(body.data._id, job._id)
+        let updatedUserJobs = await Job.find({})
+        let updatedJob = updatedUserJobs[0]
+        assert.equal(updatedJob.company, job_update_fixture.company)
+        assert.equal(updatedJob.country, job_update_fixture.country)
+        assert.equal(updatedJob.city, job_update_fixture.city)
+        assert.equal(updatedJob.notes, job_update_fixture.notes)
+        assert.equal(updatedJob.position, job_update_fixture.position)
+        assert.equal(updatedJob.priority, job_update_fixture.priority)
+        let { currency, value } = handleSalary(job_update_fixture.salary)
+        assert.equal(updatedJob.salary_currency, currency)
+        assert.equal(updatedJob.salary, value)
+        assert.equal(updatedJob.due_date.toISOString(), new Date(job_update_fixture.due_date).toISOString())
+        assert.equal(updatedJob.user_id, fixtureUserId)
+    })
+
+    it('Should delete a job', async () => {
+        const userJobs = await Job.find({})
+        const job = userJobs[0]
+        const { statusCode, body } = await request(app)
+                                        .delete(`/api/job/${job._id}`)
+                                        .set('Authorization', `Bearer ${token}`)
+
+        assert.equal(statusCode, 200)
+        assert.equal(body.success, true)
+        const deletedJob = await Job.findById(job._id) 
+        assert.exists(deletedJob.deleted_on)
+        assert.equal(deletedJob.deleted_by, fixtureUserId)
     })
 })
 
